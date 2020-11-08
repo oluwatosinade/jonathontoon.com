3528ace6f6ca500cddbcbee8959dd03d440831bb
Jonathon Toon <jonathontoon@gmail.com>
2020-11-08 22:30:23 +1100

    init

---
+++ src/scripts/index.js
@@ -0,0 +1,87 @@
+/*
+	Adapted from this initial codepen https://codepen.io/roborich/pen/wRMKaK
+*/
+
+(() => {
+	"use strict";
+	
+	/*
+		Variables
+	*/
+
+	const _faceRules = [
+		[1, 0, 2],
+		[1, 2, 0],
+		[3, 2, 1],
+		[5, 2, 3],
+		[0, 2, 5],
+		[1, 3, 4]
+	];
+
+	
+	const _faceNames = [
+		"Top",
+		"Front",
+		"Right",
+		"Back",
+		"Left",
+		"Bottom"
+	];
+
+	let _canvasBuffer = undefined;
+
+	let _faceSize = undefined;
+	let _cube = undefined;
+
+	/*
+		Functions
+	*/
+
+	const _generateFace = ([ruleR, ruleG, ruleB]) => {
+		const imageSrc = new Uint8ClampedArray(_faceSize * _faceSize * 4);
+
+		for (let i = 0; i < imageSrc.length; i += 4) {
+			const [r, g, b, a] = [i, i + 1, i + 2, i + 3];
+			const pixelI = i / 4;
+			const col = pixelI  % _faceSize;
+			const row = Math.floor(pixelI / _faceSize);
+			const ruleMap = [0, col, row, 0xFF, 0xFF - row, 0xFF - col];
+
+			imageSrc[r] = ruleMap[ruleR];
+			imageSrc[g] = ruleMap[ruleG];
+			imageSrc[b] = ruleMap[ruleB];
+			imageSrc[a] = 130;
+		}
+
+		const imageData = new ImageData(imageSrc, _faceSize, _faceSize);
+		_canvasBuffer.putImageData(imageData, 0, 0);
+		
+		const image = new Image(_faceSize, _faceSize);
+		image.src = canvas.toDataURL("image/png");
+
+		return image;
+	};
+
+	const _handleDOMContentLoaded = () => {
+		_canvasBuffer = document.getElementById("canvas").getContext("2d");
+		_cube = document.getElementById("cube");
+
+		_faceSize = 672;
+	};
+
+	const _handleWindowLoad = () => {
+		_faceRules.map(_generateFace).forEach(function (img, i) {
+			img.classList.add(`face-${i}`);
+			img.setAttribute("alt", `Cube Face ${_faceNames[i]}`);
+			_cube.appendChild(img);
+		});	
+	};
+
+	/*
+		Event Listeners
+	*/
+
+	document.addEventListener("DOMContentLoaded", _handleDOMContentLoaded, false);
+	window.addEventListener("load", _handleWindowLoad, false);
+
+})();
---
+++ src/static/service-worker.js
@@ -0,0 +1,48 @@
+const cacheName = "jonathontoon";
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./bundle.min.js",
+        "./bundle.min.css",
+        "./browserconfig.xml",
+        "./humans.txt",
+        "./images/favicon-16.png",
+        "./images/favicon-32.png",
+        "./images/favicon-48.png",
+        "./images/favicon-57.png",
+        "./images/favicon-72.png",
+        "./images/favicon-76.png",
+        "./images/favicon-96.png",
+        "./images/favicon-120.png",
+        "./images/favicon-128.png",
+        "./images/favicon-144.png",
+        "./images/favicon-152.png",
+        "./images/favicon-167.png",
+        "./images/favicon-180.png",
+        "./images/favicon-192.png",
+        "./images/favicon-192-maskable.png",
+        "./images/favicon-228.png",
+        "./images/favicon-384.png",
+        "./images/favicon-512.png",
+        "./images/favicon-512-maskable.png",
+        "./images/favicon.ico"
+      ]).then(function () { self.skipWaiting(); });
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.open(cacheName)
+        .then(function (cache) { return cache.match(e.request, { ignoreSearch: true }); })
+        .then(function (response) { return response || fetch(e.request); })
+    );
+});
